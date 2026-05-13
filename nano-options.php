250cfa5b05f4deecdadf82fef7ce5a8ef3c1f1cb
pranaykantey <pranaykanteysarker@gmail.com>
2026-05-14 00:58:20 +0600

    docs: update plugin author in header

--- nano-options.php
+++ nano-options.php
@@ -3,7 +3,7 @@
  * Plugin Name: NanoOptions
  * Description: A lightweight WordPress option framework.
  * Version: 1.0.0
- * Author: Your Name
+ * Author: pranaykantey
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: nano-options
